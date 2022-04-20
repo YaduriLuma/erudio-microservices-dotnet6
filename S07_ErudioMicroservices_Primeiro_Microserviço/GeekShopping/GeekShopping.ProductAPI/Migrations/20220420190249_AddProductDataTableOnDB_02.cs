2c7288e74ba6dd91eb5449281065565fdf1909cb
Yago <yago@lumanet.com.br>
2022-04-20 16:53:46 -0300

    Atualização S08

---
+++ S07_ErudioMicroservices_Primeiro_Microserviço/GeekShopping/GeekShopping.ProductAPI/Migrations/20220420190249_AddProductDataTableOnDB_02.cs
@@ -0,0 +1,37 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace GeekShopping.ProductAPI.Migrations
+{
+    public partial class AddProductDataTableOnDB_02 : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AlterColumn<string>(
+                name: "description",
+                table: "product",
+                type: "varchar(500)",
+                maxLength: 500,
+                nullable: true,
+                oldClrType: typeof(decimal),
+                oldType: "decimal(65,30)",
+                oldMaxLength: 500)
+                .Annotation("MySql:CharSet", "utf8mb4");
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AlterColumn<decimal>(
+                name: "description",
+                table: "product",
+                type: "decimal(65,30)",
+                maxLength: 500,
+                nullable: true,
+                oldClrType: typeof(string),
+                oldType: "varchar(500)",
+                oldMaxLength: 500)
+                .OldAnnotation("MySql:CharSet", "utf8mb4");
+        }
+    }
+}
